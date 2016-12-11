3d87c55cf2e31915e7088526fab9264eebb89211
Jakob Anderson <janderson@familysearch.org>
2016-12-10 22:50:23 -0700

    toggled author thumbs off

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,7 @@
 /* APP.JS: USE TO OVERRIDE AFTER EVERYTHING ELSE */
 Prefetch.init({
-  // containers: ['.js-nav-link', '.js-prefetch-post-title'],
   containers: ['html'],
-  // exclusions: [],        //An array of partial links passed as strings--if the potential prefetch link contains any of these partial links, it will be ignored
-  hoverDelay: 100,           //The number of miliseconds after which a sustained hover triggers a link prefetch
+  hoverDelay: 100,          //The number of miliseconds after which a sustained hover triggers a link prefetch
   enableTouch: true,        //Whether to prefetch on touchstart and therefore on mobile
   waitForMousedown: false   //Whether to prefetch on mousedown instead of on hover
 })
