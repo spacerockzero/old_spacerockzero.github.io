6bb1e69be502d967f1fc9d3bc5f89f39424ff4df
Jakob Anderson <janderson@familysearch.org>
2020-08-14 19:36:35 -0600

    removed all jekyll

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-/* APP.JS: USE TO OVERRIDE AFTER EVERYTHING ELSE */
-Prefetch.init({
-  containers: ['html'],
-  hoverDelay: 100,          //The number of miliseconds after which a sustained hover triggers a link prefetch
-  enableTouch: true,        //Whether to prefetch on touchstart and therefore on mobile
-  waitForMousedown: false   //Whether to prefetch on mousedown instead of on hover
-})
